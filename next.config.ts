525d6a9b8031340340f1f706a3a7949035bfd88b
Alok Singh <alok91340@gmail.com>
2025-06-07 00:16:27 +0530

    updated indexing

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,20 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "x-robots-tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
