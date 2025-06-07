4d77d97f8cf39f63f87657738d244633e3171ca9
Alok Singh <alok91340@gmail.com>
2025-06-07 12:34:44 +0530

    testint ssr and csr

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,4 +1,5 @@
 
+"use client";
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
